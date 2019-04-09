5db6f513310202d0e13f54d2cbcb2ba8df5265cf
Dylan Wagstaff <dylan@silverstripe.com>
2019-04-09 16:11:28 +1200

    FIX enable accessway theme only for valid actions
    Actions on the Security controller don't always return a themed response,
    nor do we want the accessway theme unanimously applied for the entire site
    as soon as the Security controller is pushed onto the controller stack.
    
    To avoid this we can defer the setting of themes until after we've
    identified that the Security controller will field the request directly,
    and that the action is one that should have the theme applied.

--- src/EnablerExtension.php
+++ src/EnablerExtension.php
@@ -5,11 +5,20 @@ namespace SilverStripe\LoginForms;
 use SilverStripe\View\SSViewer;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\Security\Security;
 
 class EnablerExtension extends DataExtension
 {
-    public function onBeforeInit()
+    public function beforeCallActionHandler()
     {
-        SSViewer::set_themes(Config::inst()->get(self::class, 'login_themes'));
+        $config = Config::inst();
+        $action = $this->owner->getAction();
+        $allowedActions = $config->get(Security::class, 'allowed_actions');
+        $excludedActions = $config->get(self::class, 'excluded_actions');
+        $themeActions = array_diff($allowedActions, $excludedActions);
+        if (in_array($action, $themeActions)) {
+            SSViewer::set_themes($config->get(self::class, 'login_themes'));
+        }
     }
 }
+
