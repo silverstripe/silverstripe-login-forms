c1bf3d5419728d1ac118df325121749f46517110
Steve Boyd <emteknetnz@gmail.com>
2021-10-27 17:59:17 +1300

    API phpunit 9 support

--- tests/php/EnablerExtensionTest.php
+++ tests/php/EnablerExtensionTest.php
@@ -12,7 +12,7 @@ class EnablerExtensionTest extends FunctionalTest
 {
     protected $usesDatabase = true;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $config = Config::modify();
